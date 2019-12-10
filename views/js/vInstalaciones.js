3eb4ed3dfcdfff19ac325a251c2315c08e360a1e
unknown <inakillorens9@gmail.com>
2019-12-10 11:06:17 +0100

    cambios ligeros y modificacion instalaciones

--- views/js/vInstalaciones.js
+++ views/js/vInstalaciones.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
   
     
 
-
-$(".col-md-4").on("hover", function(){
-
+$(".col-md-4").on("mouseover", function(){
+$(this).children(".descripcion").slideDown("fast");
     
-})
+});
+
+$(".col-md-4").on("mouseleave", function(){
+    $(this).children(".descripcion").slideUp("fast");
+    
+    });
 
 
 
