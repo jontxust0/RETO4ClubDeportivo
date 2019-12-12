32e0c6042b5b56600b619b9de716f469c67f7be1
unknown <inakillorens9@gmail.com>
2019-12-12 10:16:41 +0100

    enseñar jugadores

--- model/equipoModel.php
+++ model/equipoModel.php
@@ -1,6 +1,12 @@
 <?php 
 
+include_once "jugadoresModel.php";
+include_once "entrenadoresModel.php";
+
 class equipoModel extends equipoClass{
+    private $list = array();
+    private $arrJugadores = array();
+    private $objEntrenador = new JugadoresModel;
     
 }
 
