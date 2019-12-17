358f003a76f93c859b21cf8261b668b0dbc7fbcd
inakiLlorens <inakillorens9@gmail.com>
2019-12-17 18:28:56 +0100

    ligeros cambios e imprimir el cuerpo medico

--- views/js/vJugadores.js
+++ views/js/vJugadores.js
@@ -56,11 +56,12 @@ $(document).ready(function(){
 				   
 			
 			   htmlzatia+=`
-			<h1>`+result[equipo].nombre+`</h1>
+			
 			   <div class="infoEquipos">
+			   <h1 class="teamName">`+result[equipo].nombre+`</h1>
 				<div class="row">
-					<div class="infoJugadores col">
-					<h3>Jugadores</h3>
+					<div class="infoJugadores col-lg-8">
+					<h3 class="nameCat">Jugadores</h3>
 					<div class="row card-container">
 					`;
 					//imprimir cards de jugadores
@@ -70,9 +71,9 @@ $(document).ready(function(){
 						<div class="card-body">
 						  <h5 class="card-title">`+result[equipo].listJugadores[jugador].objUsuario.name+`, `+result[equipo].listJugadores[jugador].objUsuario.surname+`</h5>
 							<ul>
-								<li><b>Posición:</b> `+result[equipo].listJugadores[jugador].posicion+`</li>
-								<li><b>Dorsal:</b>`+result[equipo].listJugadores[jugador].dorsal+`</li>
-								<li><b>Telefono:</b>`+result[equipo].listJugadores[jugador].tlf+`</li>
+								<li><b>Posición: </b> `+result[equipo].listJugadores[jugador].posicion+`</li>
+								<li><b>Dorsal: </b>`+result[equipo].listJugadores[jugador].dorsal+`</li>
+								<li><b>Telefono: </b>`+result[equipo].listJugadores[jugador].tlf+`</li>
 								<li><b></b></li>
 							</ul>
 						</div>
@@ -87,34 +88,55 @@ $(document).ready(function(){
 					</div> 
 		   		
 			
-					<div class="cuerpoTecnico col col-lg-2">
-					<h3>Cuerpo tecnico</h3>
+					<div class="cuerpoTecnico col">
+					<h3 class="nameCat">Cuerpo tecnico</h3>
 						<div class="infoEntrenadores">
 						<h4>Entrenadores</h4>
 
 						<div class="row card-container">
 					`;
+					//Imprimir cards de todos los entrenadores
 					for (let entrenador = 0; entrenador < result[equipo].listEntrenadores.length; entrenador++) {
 						htmlzatia+=`<div class="card col-md-16" style="width: 18rem;">
 						<img src="`+result[equipo].listEntrenadores[entrenador].objUsuario.pic+`" class="card-img-top" alt="...">
 						<div class="card-body">
 						  <h5 class="card-title">`+result[equipo].listEntrenadores[entrenador].objUsuario.name+`, `+result[equipo].listEntrenadores[entrenador].objUsuario.surname+`</h5>
 							<ul>
-								<li><b>Sueldo:</b> `+result[equipo].listEntrenadores[entrenador].sueldo+`</li>
-								<li><b>Fecha contratacion:</b>`+result[equipo].listEntrenadores[entrenador].fechaContratacion+`</li>
-								<li><b>Telefono:</b>`+result[equipo].listEntrenadores[entrenador].tlf+`</li>
+								<li><b>Sueldo: </b> `+result[equipo].listEntrenadores[entrenador].sueldo+`</li>
+								<li><b>Fecha contratacion: </b>`+result[equipo].listEntrenadores[entrenador].fechaContratacion+`</li>
+								<li><b>Telefono: </b>`+result[equipo].listEntrenadores[entrenador].tlf+`</li>
 								<li><b></b></li>
 							</ul>
 						</div>
 					  </div>`;
 						
 					}
+					//--------------------------------------
 					htmlzatia+=`
 					</div>
 		   				</div>
 						<div class="InfoCuerpo ">
 						<h4>Cuerpo medico</h4>   
 
+						<div class="row card-container">
+					`;
+					//imprimir cards de todos los del cuerpo medico
+					for (let cuerpo = 0; cuerpo < result[equipo].listCuerpo.length; cuerpo++) {
+						htmlzatia+=`<div class="card col-md-16" style="width: 18rem;">
+						<img src="`+result[equipo].listCuerpo[cuerpo].objUsuario.pic+`" class="card-img-top" alt="...">
+						<div class="card-body">
+						  <h5 class="card-title">`+result[equipo].listCuerpo[cuerpo].objUsuario.name+`, `+result[equipo].listCuerpo[cuerpo].objUsuario.surname+`</h5>
+							<ul>
+								<li><b>Funcion: </b> `+result[equipo].listCuerpo[cuerpo].funcion+`</li>
+								<li><b>Telefono: </b>`+result[equipo].listCuerpo[cuerpo].tlf+`</li>
+								<li><b></b></li>
+							</ul>
+						</div>
+					  </div>`;
+						
+					}
+					//----------------------------------------
+					htmlzatia+=`
 					</div>
 					</div>
 		   </div>
