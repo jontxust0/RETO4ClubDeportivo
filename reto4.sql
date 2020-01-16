0d763d0c4fe10e000761f9deb64e83ee55fef260
unknown <inakillorens9@gmail.com>
2020-01-16 08:56:54 +0100

    bbdd

--- reto4.sql
+++ reto4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-01-2020 a las 14:08:30
+-- Tiempo de generación: 16-01-2020 a las 08:56:27
 -- Versión del servidor: 10.4.6-MariaDB
 -- Versión de PHP: 7.3.9
 
