a6335698bae70eebfcb4691760195f62a243eb8e
unknown <inakillorens9@gmail.com>
2020-01-22 14:51:54 +0100

    commit

--- reto4.sql
+++ reto4.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-01-2020 a las 12:25:03
+-- Tiempo de generación: 22-01-2020 a las 14:51:27
 -- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.1.32
+-- Versión de PHP: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -53,6 +53,9 @@ SELECT * FROM user$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spAllVotos` ()  NO SQL
 SELECT * FROM votos$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spCheckCuerpoMedico` (IN `inId` INT)  NO SQL
+SELECT * FROM `cuerpomedico` WHERE inId=id_usuario$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spCheckVote` (IN `idUser` INT, IN `idCat` INT)  NO SQL
 SELECT * FROM `votos` WHERE id_usuario=idUser AND id_categoria=idCat$$
@@ -173,7 +176,6 @@ INSERT INTO `categorias` (`id`, `nombre`, `cuota`) VALUES
 (2, 'Cadete', '12.70'),
 (3, 'juvenil', '14.10'),
 (4, 'Senior', '17.80'),
-(5, 'juvenil', '10.50'),
 (6, 'proaso', '1.00');
 
 -- --------------------------------------------------------
@@ -196,7 +198,12 @@ CREATE TABLE `cuerpomedico` (
 --
 
 INSERT INTO `cuerpomedico` (`id`, `funcion`, `direccion`, `tlf`, `id_usuario`, `id_equipo`) VALUES
-(1, 'Fisio', 'Lekeitio', '658965478', 29, 1);
+(1, 'Fisio', 'Lekeitio', '658965478', 18, 1),
+(4, 'Psicologo', 'Durango', '667123907', 19, 1),
+(5, 'Fisio', 'Bilbao', '686546012', 17, 2),
+(6, 'Psicologo', 'Bermeo', '66513442', 22, 3),
+(7, 'Fisio', 'Gernika', '662109803', 21, 3),
+(8, 'Psicologo', 'Zornotza', '634999022', 20, 2);
 
 -- --------------------------------------------------------
 
@@ -217,7 +224,8 @@ CREATE TABLE `datosmedicos` (
 --
 
 INSERT INTO `datosmedicos` (`id`, `lesiones`, `tipoSangre`, `enfermedades`, `id_jugador`) VALUES
-(1, 'Rotura de femur', 'b-', 'Asma', 1);
+(1, 'Rotura de femur', 'B-', 'Asma', 2),
+(2, 'Traumatismo leve', 'B-', 'Ninguna', 3);
 
 -- --------------------------------------------------------
 
@@ -240,7 +248,10 @@ CREATE TABLE `entrenadores` (
 --
 
 INSERT INTO `entrenadores` (`id`, `tlf`, `direccion`, `sueldo`, `fechaContratacion`, `id_usuario`, `id_equipo`) VALUES
-(1, '652632452', 'Lekeitio', '2005', '2020-01-20 11:38:04', 29, 2);
+(1, '652632452', 'Bermeo', '2005', '2019-12-18 07:41:18', 2, 1),
+(3, '644120334', 'Bilbao', '2500', '2019-12-20 08:00:08', 14, 3),
+(7, '655172879', 'Zornotza', '2900', '2019-12-19 13:30:45', 23, 2),
+(8, '123123123', 'Lekeitio', '0', '2019-12-20 08:01:15', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -260,8 +271,9 @@ CREATE TABLE `equipos` (
 --
 
 INSERT INTO `equipos` (`id`, `femenino/masculino`, `nombre`, `id_categoria`) VALUES
-(1, 'F', 'juvenil B', 5),
-(2, 'F', 'TuPuta', 5);
+(1, 'F', 'juvenil B', 3),
+(2, 'M', 'Bilbao basket B', 4),
+(3, 'F', 'Zornotza Saski', 1);
 
 -- --------------------------------------------------------
 
@@ -272,6 +284,7 @@ INSERT INTO `equipos` (`id`, `femenino/masculino`, `nombre`, `id_categoria`) VAL
 CREATE TABLE `fotosequipo` (
   `id` int(11) NOT NULL,
   `privado` tinyint(1) NOT NULL DEFAULT 0,
+  `name` varchar(50) COLLATE utf8_bin NOT NULL,
   `pic` varchar(254) COLLATE utf8_bin NOT NULL,
   `id_equipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -280,13 +293,8 @@ CREATE TABLE `fotosequipo` (
 -- Volcado de datos para la tabla `fotosequipo`
 --
 
-INSERT INTO `fotosequipo` (`id`, `privado`, `pic`, `id_equipo`) VALUES
-(1, 0, 'img/imgPublico1.jpg\r\n', 1),
-(2, 0, 'img/imgPublico2.jpg', 1),
-(3, 0, 'img/imgPublico3.jpg', 2),
-(4, 1, 'img/imgPublico4.jpg', 1),
-(5, 1, 'img/imgPublico5.jpg', 1),
-(6, 1, 'img/imgPublico6.jpg', 1);
+INSERT INTO `fotosequipo` (`id`, `privado`, `name`, `pic`, `id_equipo`) VALUES
+(1, 0, '', 'img/imgPublico1.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -311,8 +319,20 @@ CREATE TABLE `jugadores` (
 --
 
 INSERT INTO `jugadores` (`id`, `direccion`, `dorsal`, `posicion`, `tlf`, `altura`, `id_datosMedicos`, `id_usuario`, `id_equipo`) VALUES
-(1, 'Lekeitio', 12, 'alero', '343434333', '192.00', 1, 28, 1),
-(2, 'asdas', 11, 'alero', '1111111', '188.00', 1, 30, 1);
+(1, 'Avenida Sin Nombre ', 12, 'medio', '94654654', '1.59', 1, 2, 1),
+(2, 'Avenida Ceda el paso', 14, 'alero', '123123123', '1.90', 2, 3, 2),
+(3, 'Calle Falsa 123', 99, 'central', '666666666', '1.00', NULL, 1, 2),
+(4, 'Planeta Tsufur', 17, 'Defensa', '666345123', '1.70', NULL, 7, 3),
+(5, 'Even green Terrace 10', 19, 'Alero', '689002792', '1.64', NULL, 9, 2),
+(6, 'Elk river', 66, 'alero', '644420925', '1.74', NULL, 11, 2),
+(7, 'Ciudad Endrino', 18, 'Defensa', '623902398', '1.79', NULL, 8, 1),
+(8, 'Calle los Santos 8', 8, 'Delantero', '666345790', '1.82', NULL, 5, 3),
+(10, 'Barnekalea 27', 5, 'Alero', '666457123', '1.89', NULL, 6, 1),
+(11, 'Zornotza', 13, 'Delantero', '663', '1.79', 2, 27, 1),
+(12, 'Gamiz-Fika', 12, 'Defensa', '1.72', '1.00', 2, 25, 1),
+(13, 'Bermeo', 7, 'Delantero', '662879001', '1.79', 1, 28, 3),
+(14, 'Zornotza', 9, 'Defensa', '662012366', '1.76', 1, 25, 2),
+(15, 'Bermeo', 6, 'Alero', '66298772', '1.76', 2, 25, 2);
 
 -- --------------------------------------------------------
 
@@ -358,14 +378,41 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`idUser`, `username`, `password`, `name`, `surname`, `email`, `admin`, `pic`) VALUES
-(26, 'userprueba', '', 'user', 'prueba', 'user@gmail.com', 0, 'predeterminado.png'),
-(27, 'aaa', '$2y$10$v0ol6pRtlxNu7tcyvfVi3ei7FfjXG4fhzgC6fLo3GoJFaRLfKrPTe', 'aaa', 'aaa', 'aaa', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
-(28, 'gotzon95', '$2y$10$H7PhAgZ6RNSiAOeMNc0SsuawLhofoRLF8eiRoO1/zoPoqGilbpSRu', 'Gotzon', 'Galletebeitia', 'gotzon@gmail.com', 1, '228130715ee841a5e2528de6e3ae1820.jpg'),
-(29, 'xarles', '$2y$10$mE69YDOZAMmQ36OaMRVMdeXUdaDf2oNsRIjkXrn80kKkIGa34mZyi', 'Xarles', 'Goitiz', 'xarles@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
-(30, 'bbb', '$2y$10$/BnTaXzShVsmqgva2cwuvOe/vhvJHtT2sLE2dzrBMl5emJMei94Li', 'wsdf', 'sdfsdf', 'sdf', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
-(31, 'ddd', 'ddd', 'dd', 'ddd', 'dd', 1, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
-(32, 'sss', '$2y$10$Dqco76TV3lzVK8oTsDJt0Op6fGMDhtdXsXMKCDPGBZiG7FI33GbWe', 'ss', 'ss', 'ss', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
-(50, 'zzz', '$2y$10$gZDNWfSmCaK40JmC23gsjuZA8L58yLoRKssnVoy7xjNSxYkWP03om', 'zzz', 'zzz', 'zzz@zzz.zzz', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png');
+(1, 'AdminUser', 'AdminUser', 'Carl', 'Johnson', 'CJ@gmail.com', 1, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(2, 'jone_12', '$2y$10$v0ol6pRtlxNu7tcyvfVi3ei7FfjXG4fhzgC6fLo3GoJFaRLfKrPTe', 'Jone', 'Etxandio', 'jone_12@gmail.es', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(3, 'gotzon95', '$2y$10$H7PhAgZ6RNSiAOeMNc0SsuawLhofoRLF8eiRoO1/zoPoqGilbpSRu', 'Gotzon', 'Galletebeitia', 'gotzon@gmail.com', 1, 'https://upload.wikimedia.org/wikipedia/commons/9/96/Kobe_Bryant_8.jpg'),
+(4, 'xarles', '$2y$10$mE69YDOZAMmQ36OaMRVMdeXUdaDf2oNsRIjkXrn80kKkIGa34mZyi', 'Xarles', 'Goitiz', 'xarles@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(5, 'markel84', '$2y$10$/BnTaXzShVsmqgva2cwuvOe/vhvJHtT2sLE2dzrBMl5emJMei94Li', 'Markel', 'Fernandez', 'markel84@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(6, 'Aitortilla19', 'S0lñW?#1849-aSW', 'Aitor', 'Ercoreca', 'Aitortilla19@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(7, 'viciauSP', '1267Ex?ññp&.', 'Horacio Jose', 'De la cruz', 'viciauSP-1@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(8, 'Mikel_domin', '57284753049AWD2', 'Mikel', 'Dominguez', 'mikel_domin@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(9, 'meraS98HD', '4534PSÑ?¿12A', 'Maria', 'Etxebarria', 'meraS98HD@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(10, 'noMires', '45647685745AS', 'Miranda', 'Rodriguez', 'noMires@gmail.es', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(11, 'Awthdrew14', '12443=¿', 'Andrew', 'Harrington', 'Awthdrew14@outlook.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(12, 'Carlos83', '4324255er?!', 'Carlos', 'Martin', 'CarlosM84@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(13, 'PeiGoi87', '3467WD#er', 'Peio', 'Goikoetxea', 'PeiGoi87@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(14, 'Miguel', '2345432SWF', 'Miguel', 'Casado', 'Miguelkasdelimon@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(15, 'BorjaHK', '3453R4ERF', 'Borja', 'Hidalgo', 'BorjaH@gmail.es', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(16, 'Jperez345', '5463563423de', 'Jose', 'Perez', 'Jperez345@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(17, 'Ane_uribe', '435254eefd233', 'Ane', 'Uribe', 'Aneuribe88@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(18, 'marcos', '$2y$10$H7PhAgZ6RNSiAOeMNc0SsuawLhofoRLF8eiRoO1/zoPoqGilbpSRu', 'Marcos', 'Fernandez', 'MarcosF@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(19, 'Juanrush3', '3456rtyu', 'Juan', 'Ivanov', 'Juanrush3@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(20, 'asMar', '34564SX', 'Asier', 'Martinez', 'asMar@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(21, 'MPsicolog', '234543SZ', 'Mireia', 'Saez', 'MPsicolog@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(22, 'AMontes@gmail.com', '234543es', 'Andres', 'Montes', 'AMontes', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(23, 'GLolbah', '234472sx', 'Gorka', 'Larrabeiti', 'GLolbah@gmail.com', NULL, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(24, 'And84', '2456sw', 'Ander', 'Uriarte', 'An84@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(25, 'Iñaki90', '5364748', 'Iñaki', 'Garcia', 'Iñaki90mer@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(26, 'Nerea18', '123986xd', 'Nerea', 'Vazquez', 'Nerea18@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(27, 'Eder_barrena', '35464ES', 'Eder', 'Barrena', 'Ederbarrena@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(28, 'Carlos_rodri', '532435iu', 'Carlos', 'Rodriguez', 'carlos_rodriguez@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(29, 'JasminaV', '35647', 'Jasmina', 'Valdes', 'JasminaV@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(30, 'MG998', '12345re', 'Maria', 'Gutierrez', 'MG998@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(31, 'iker_astorkia', 'rr¡455tegd?', 'Iker', 'Astorkia', 'iker_astorkia@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(32, 'Mateomate', '7823sx', 'Mateo', 'Santos', 'Mateomate@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(33, 'Aitor_arranz', '74633WED!!', 'Aitor', 'Arranz', 'Aitor_arranz@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(34, 'Bartolo', '$2y$10$H7PhAgZ6RNSiAOeMNc0SsuawLhofoRLF8eiRoO1/zoPoqGilbpSRu', 'Bartolo', 'Bartolo', 'Bartolo@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png'),
+(35, 'pruebaAAA', '$2y$10$C6yDKRDaQLX2DymFR1uI9eEVcWWCe55L0LrGik/pn4A5LrFlFqW6.', 'inaki', 'Llorens', 'inakillorens9@gmail.com', 0, 'https://www.stickpng.com/assets/images/585e4bf3cb11b227491c339a.png');
 
 -- --------------------------------------------------------
 
@@ -421,6 +468,13 @@ ALTER TABLE `equipos`
   ADD KEY `id_categoria` (`id_categoria`);
 
 --
+-- Indices de la tabla `fotosequipo`
+--
+ALTER TABLE `fotosequipo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_equipo` (`id_equipo`);
+
+--
 -- Indices de la tabla `jugadores`
 --
 ALTER TABLE `jugadores`
@@ -442,6 +496,15 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`idUser`);
 
 --
+-- Indices de la tabla `votos`
+--
+ALTER TABLE `votos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_categorias` (`id_categoria`),
+  ADD KEY `id_jugadorVotado` (`id_jugadorVotado`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -455,31 +518,37 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `cuerpomedico`
 --
 ALTER TABLE `cuerpomedico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `datosmedicos`
 --
 ALTER TABLE `datosmedicos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `entrenadores`
 --
 ALTER TABLE `entrenadores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `equipos`
 --
 ALTER TABLE `equipos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `fotosequipo`
+--
+ALTER TABLE `fotosequipo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `jugadores`
 --
 ALTER TABLE `jugadores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `quejas`
@@ -491,7 +560,13 @@ ALTER TABLE `quejas`
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT de la tabla `votos`
+--
+ALTER TABLE `votos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- Restricciones para tablas volcadas
@@ -524,11 +599,25 @@ ALTER TABLE `equipos`
   ADD CONSTRAINT `equipos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Filtros para la tabla `fotosequipo`
+--
+ALTER TABLE `fotosequipo`
+  ADD CONSTRAINT `fotosequipo_ibfk_1` FOREIGN KEY (`id_equipo`) REFERENCES `equipos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `jugadores`
 --
 ALTER TABLE `jugadores`
   ADD CONSTRAINT `jugadores_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `jugadores_ibfk_2` FOREIGN KEY (`id_equipo`) REFERENCES `equipos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `votos`
+--
+ALTER TABLE `votos`
+  ADD CONSTRAINT `votos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `votos_ibfk_2` FOREIGN KEY (`id_jugadorVotado`) REFERENCES `jugadores` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `votos_ibfk_3` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
